295028d5519b1166b24b67257147a108c0b38d52
ThaYT <81139511+ThaYTOfficial@users.noreply.github.com>
2024-08-12 15:57:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "started"
 git clone https://github.com/foxytouxxx/freeroot.git
+apt update
+apt install sudo
 cd freeroot
 bash root.sh
 
+sudo apt-get install systemd
 sudo apt install -y curl wget git python3
 sudo curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
 sudo chmod -R 777 /bin/systemctl
-sudo apt-get install systemd
+
