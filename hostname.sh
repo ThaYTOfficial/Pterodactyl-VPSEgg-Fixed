ae5e6b2012847676f4e0654fc2c7c0049ecf7a3c
ThaYT <81139511+ThaYTOfficial@users.noreply.github.com>
2024-12-05 00:11:09 +0530

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -1,4 +1,4 @@
-echo 'export HOSTNAME="VM"' >> ~/.bashrc
+echo 'export HOSTNAME="vps4uwu"' >> ~/.bashrc
 
 cat >> ~/.bashrc <<-'EOF'
 PS1='\[\e]0;\u@$HOSTNAME: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$HOSTNAME\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
