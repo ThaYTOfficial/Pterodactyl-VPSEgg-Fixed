dc5031126b740395e250c62f5b7d29eece21cab8
ThaYT <81139511+ThaYTOfficial@users.noreply.github.com>
2024-12-04 15:27:59 +0530

    Create hostname.sh

---
+++ hostname.sh
@@ -0,0 +1,6 @@
+echo 'export HOSTNAME="VM"' >> ~/.bashrc
+
+cat >> ~/.bashrc <<-'EOF'
+PS1='\[\e]0;\u@$HOSTNAME: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@$HOSTNAME\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+EOF
+source ~/.bashrc
