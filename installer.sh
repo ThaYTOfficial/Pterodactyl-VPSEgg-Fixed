c1ef13518caa0707fae40fdd432bf6bee42c8f2f
ThaYT <81139511+ThaYTOfficial@users.noreply.github.com>
2025-04-27 23:10:15 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -32,10 +32,9 @@ ${bold}${lightblue}:!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!  
 ${bold}${lightblue} ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     ::        
 ${bold}${lightblue} :           :     : :: ::    :   : :   : :  :         :       :      :          
                                                                                                   
-                                                                                                                
 ${bold}${lightgreen}========================================================================
- "
- 
+"
+
 echo "${nc}"
 
 if [[ -f "./installed" ]]; then
@@ -55,44 +54,50 @@ if [[ -f "./installed" ]]; then
     runcmd
 else
     echo "Downloading files for application"
+    
     curl -sSLo ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>err.log
     echo -ne '#                   (5%)\r'
-    curl -sSLo files.zip https://github.com/RealTriassic/Ptero-VM-JAR/releases/download/latest/files.zip >/dev/null 2>err.log
-    echo -ne '##                  (10%)\r'
-    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
-    echo -ne '####                (20%)\r'
-    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
-    echo -ne '#####               (25%)\r'
-    chmod +x unzip >/dev/null 2>err.log
-    export PATH="/bin:/usr/bin:/usr/local/bin:/sbin:$HOMEA/bin:$HOMEA/usr/bin:$HOMEA/sbin:$HOMEA/usr/sbin:$HOMEA/etc/init.d:$PATH"
-    ./unzip ngrok.zip >/dev/null 2>err.log
-    echo -ne '######               (30%)\r'
-    ./unzip files.zip >/dev/null 2>err.log
-    echo -ne '#######              (35%)\r'
-    ./unzip root.zip
-    tar -xf root.tar.gz >/dev/null 2>err.log
-    echo -ne '########             (40%)\r'
-    chmod +x ./libraries/proot >/dev/null 2>err.log
-    echo -ne '#########            (45%)\r'
-    chmod +x ngrok >/dev/null 2>err.log
-    echo -ne '##########           (50%)\r'
-    chmod +x gotty >/dev/null 2>err.log
-    echo -ne '###########          (55%)\r'
-    rm -rf files.zip >/dev/null 2>err.log
-    rm -rf root.zip >/dev/null 2>err.log
-    rm -rf root.tar.gz >/dev/null 2>err.log
-    rm -rf ngrok.zip >/dev/null 2>err.log
-    echo -ne '############         (60%)\r'
 
-    cmds=("mv gotty /usr/bin/" "mv unzip /usr/bin/" "mv ngrok /usr/bin/" "apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
+    # Download Ubuntu 22.04 root filesystem
+    curl -sSLo root.tar.xz https://partner-images.canonical.com/core/jammy/current/ubuntu-jammy-core-cloudimg-amd64-root.tar.xz >/dev/null 2>err.log
+    echo -ne '####                (20%)\r'
+
+    curl -sSLo unzip https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/unzip >/dev/null 2>err.log
+    echo -ne '#####               (25%)\r'
+
+    curl -sSLo gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty >/dev/null 2>err.log
+    chmod +x unzip gotty >/dev/null 2>err.log
+    echo -ne '######               (30%)\r'
+
+    # Extract Ubuntu 22.04 rootfs
+    mkdir -p ubuntu22
+    tar -xf root.tar.xz -C ubuntu22 >/dev/null 2>err.log
+    echo -ne '########             (40%)\r'
+
+    mv ubuntu22/* . >/dev/null 2>err.log
+    rm -rf ubuntu22 root.tar.xz
+    echo -ne '#########            (45%)\r'
+
+    chmod +x ./libraries/proot >/dev/null 2>err.log
+    ./unzip ngrok.zip >/dev/null 2>err.log
+    rm -rf ngrok.zip >/dev/null 2>err.log
+
+    echo -ne '##########           (50%)\r'
+    mv gotty /usr/bin/
+    mv unzip /usr/bin/
+    mv ngrok /usr/bin/
+    echo -ne '###########          (55%)\r'
+
+    cmds=("apt-get update" "apt-get -y upgrade" "apt-get -y install sudo curl wget hwloc htop nano neofetch python3" "curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py")
 
     for cmd in "${cmds[@]}"; do
         ./libraries/proot -S . /bin/bash -c "$cmd >/dev/null 2>err.log"
     done
+
     echo -ne '####################(100%)\r'
     echo -ne '\n'
     touch installed
-    
+
     echo "
 ${bold}${lightgreen}========================================================================
                                                                                                   
@@ -107,11 +112,10 @@ ${bold}${lightblue}:!:         :!:    :!:       :!:  !:!  :!:  !:!      ::!!:!  
 ${bold}${lightblue} ::          ::     :: ::::  ::   :::  ::::: ::       ::::    :::     ::        
 ${bold}${lightblue} :           :     : :: ::    :   : :   : :  :         :       :      :          
                                                                                                   
-                                                                                                                
 ${bold}${lightgreen}========================================================================
- "
+"
  
-echo "${nc}"
+    echo "${nc}"
     
     echo "${bold}${lightgreen}==> Started ${lightblue}Container${lightgreen} <=="
     function runcmd1 {
